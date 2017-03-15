a8c027e3c2c9bf5307ff803b992d57d30eb0f240
daebwae <daebwae@gmail.com>
2017-03-15 23:57:10 +0100

    Preserve existing AIs - deny replacement
    The code allows for replacing AIs later at any time. However we need ACL for that.

--- routes/v1/perceptions/perceptions-ai.js
+++ routes/v1/perceptions/perceptions-ai.js
@@ -9,31 +9,29 @@ passport.use(require("../../../middleware/passport-token").TokenStrategy);
 var PerceptionsAiService = require("../../../services/perceptions/perceptions-ai-service");
 var ValidationError = require("../../../utils/converter/validation-error");
 
-router.route("/ai")
+router.route("/ai/:guid")
   .post(passport.authenticate("token",{session:false}), (req, res) => {
 
-    var dBParams = {
-
+    var params = {
       minimal_detected_windows: req.body.minimal_detected_windows,
       minimal_detection_confidence: req.body.minimal_detection_confidence,
       shortname: req.body.shortname,
       event_type: req.body.event_type,
-      event_value: req.body.event_value
+      event_value: req.body.event_value,
+      guid: req.params.guid,
+      archive: req.files.archive.path
     };
 
-    var uploadParams = {
-      file: req.files.ai.path
-    };
 
-    var resultAi = null;
 
-    PerceptionsAiService.createAiDb(dBParams)
-      .then(ai => {uploadParams.guid=ai.guid; resultAi = ai})
-      .then(() => {return PerceptionsAiService.uploadAi(uploadParams)})
-      .then(() => res.status(200).json({success: true, shortname: resultAi.shortname, ai_guid: resultAi.guid, minimal_detection_confidence: resultAi.minimal_detection_confidence, minimal_detected_windows: resultAi.minimal_detected_windows}))
+    PerceptionsAiService.createAi(params)
+      .then(created => res.status(200).json({
+        shortname: params.shortname, guid: params.guid,
+        minimal_detection_confidence: params.minimal_detection_confidence,
+        minimal_detected_windows: params.minimal_detected_windows, created: created}))
       .catch(ValidationError, e => httpError(res, 400, null, e.message))
       // catch-all for any other that is not based on user input
-      .catch(e => httpError(res, 500, e, "Sensations couldn't be created."));
+      .catch(e => httpError(res, 500, e, `Perception Ai couldn't be created: ${e}`));
 
   });
 module.exports = router;
